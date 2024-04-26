c3e89f764059ce45c188d5ef6a0999840a47be44
Hob Adams <hob.adams@motivepartners.com>
2024-04-26 13:45:38 +0100

    tidy up button

--- app/page.tsx
+++ app/page.tsx
@@ -28,17 +28,22 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <Link href="/api/test/python">
-            <code className="font-mono font-bold">api/index.py</code>
-          </Link>
-          <Link href="/api/post">
-            <code className="font-mono font-bold">api/test.py</code>
-          </Link>
+        <div>
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Get started by editing&nbsp;
+            <Link href="/api/test/python">
+              <code className="font-mono font-bold">api/index.py</code>
+            </Link>
 
-          <button onClick={postData}>Click me</button>
-        </p>
+          </p>
+        </div>
+
+        <div>
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <button onClick={postData}>Click me  to try post</button>
+          </p>
+        </div>
+
         {response && (
           <div>
             <h2>Response:</h2>
